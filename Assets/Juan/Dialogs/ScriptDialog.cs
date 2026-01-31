fd5483a95a1a59b1df5bde53c3d28dd8b7c2a358
Araanda41 <teamzzesp@gmail.com>
2026-01-31 23:26:07 +0100

    Dialogos mejor  y 2 sprites

--- Assets/Juan/Dialogs/ScriptDialog.cs
+++ Assets/Juan/Dialogs/ScriptDialog.cs
@@ -1,21 +1,26 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic; // Necesario para listas
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine.InputSystem;
+
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public TextMeshProUGUI textComponent;
     public float textSpeed = 0.05f;
 
-    public List<SintomaData> baseDeDatosSintomas; 
+    public List<SintomaData> baseDeDatosSintomas;
 
-    public List<Enfermedad> listaEnfermedades;
+    public List<Enfermedad.Enfermedades> enfermedadesGraves;
+    public List<Enfermedad.Enfermedades> enfermedadesLeves;
 
-    private List<string> lines = new List<string>(); 
+    public bool leve = false; 
+    public Enfermedad.Enfermedades nakim; 
+
+    private List<string> lines = new List<string>();
     private int index;
-
+    private  Enfermedad enfermedad;
 
     public enum TipoSintoma { A_Calor, B_Flujos, C_Cuerpo, D_Dolor, E_Conducta }
 
@@ -25,17 +30,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public struct SintomaData
     {
         public TipoSintoma tipo;
-        public TextAsset archivoTexto; 
+        public TextAsset archivoTexto;
     }
 
-    [System.Serializable]
-    public struct Enfermedad
-    {
-        public Enfermedades nombre;
-        public TipoSintoma[] combinacion;
-    }
-
-    
+ 
 
     void Start()
     {
@@ -64,15 +62,31 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         lines.Clear();
 
-        // enfermedad Random
-        if (listaEnfermedades.Count == 0) return;
-        Enfermedad enfermedadElegida = listaEnfermedades[Random.Range(0, listaEnfermedades.Count)];
+        Enfermedad.Enfermedades enfermedadElegida;
+
+        if (leve)
+        {
+            if (enfermedadesLeves.Count == 0)
+            {
+                return;
+            }
+            enfermedadElegida = enfermedadesLeves[UnityEngine.Random.Range(0, enfermedadesLeves.Count)];
+        }
+        else
+        {
+            if (enfermedadesGraves.Count == 0)
+            {
+                return;
+            }
+            enfermedadElegida = enfermedadesGraves[UnityEngine.Random.Range(0, enfermedadesGraves.Count)];
+        }
+
+        nakim = enfermedadElegida;
         
 
-        List<TipoSintoma> sintomasMezclados = new List<TipoSintoma>(enfermedadElegida.combinacion);
-        Shuffle(sintomasMezclados); 
+        List<Enfermedad.TipoSintoma> sintomasMezclados = new List<Enfermedad.TipoSintoma>(enfermedad.getSintomas(nakim));
+        Shuffle(sintomasMezclados);
 
-        // de cada síntoma saco archivo y una frase random
         foreach (TipoSintoma tipo in sintomasMezclados)
         {
             string frase = ObtenerFraseRandomDeSintoma(tipo);
@@ -85,43 +99,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     string ObtenerFraseRandomDeSintoma(TipoSintoma tipoBuscado)
     {
-        // busco el textAsset
         foreach (var data in baseDeDatosSintomas)
         {
             if (data.tipo == tipoBuscado && data.archivoTexto != null)
             {
-                // separar el txt por líneas
                 string[] posiblesFrases = data.archivoTexto.text.Split(new char[] { '\n', '\r' }, System.StringSplitOptions.RemoveEmptyEntries);
-                
+
                 if (posiblesFrases.Length > 0)
                 {
-                    // frase random de este archivo
-                    return posiblesFrases[Random.Range(0, posiblesFrases.Length)];
+                    return posiblesFrases[UnityEngine.Random.Range(0, posiblesFrases.Length)];
                 }
             }
         }
         return "Error: No se encontró texto para el síntoma " + tipoBuscado;
     }
 
-    // barajar listas
     void Shuffle<T>(List<T> list)
     {
         int n = list.Count;
         while (n > 1)
         {
             n--;
-            int k = Random.Range(0, n + 1);
+            int k = UnityEngine.Random.Range(0, n + 1);
             T value = list[k];
             list[k] = list[n];
             list[n] = value;
         }
     }
 
-
     void comenzarDialogo()
     {
         index = 0;
-        if(lines.Count > 0)
+        if (lines.Count > 0)
             StartCoroutine(Typeline());
     }
 
