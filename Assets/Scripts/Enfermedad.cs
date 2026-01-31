a0cd3b8897a94be34902ef23d0e670b0fb487b34
notNoe13 <92950852+rubeneitor21@users.noreply.github.com>
2026-01-31 15:02:17 +0100

    Add Enfermedad script mapping diseases to symptoms
    Introduce a new Unity MonoBehaviour Enfermedad that defines TipoSintoma and Enfermedades enums and a dictionary mapping each disease to an array of symptom types. The mapping is populated in Start(), there's a public getSintomas(Enfermedades) accessor, and an empty Update() placeholder. Also adds the corresponding .meta file.

---
+++ Assets/Scripts/Enfermedad.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enfermedad : MonoBehaviour
+{
+
+    public enum TipoSintoma { A_Calor, B_Flujos, C_Cuerpo, D_Dolor, E_Conducta }
+    public enum Enfermedades { CATARRO, SARPULLIDO, RAMAS_BRAZOS, OTITIS, OJOS_CARACOL, LEPRA, BICHOS_OJOS }
+
+    Dictionary<Enfermedades, TipoSintoma[]> enfermedades = new Dictionary<Enfermedades, TipoSintoma[]>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        enfermedades[Enfermedades.CATARRO] = new TipoSintoma[] { TipoSintoma.A_Calor, TipoSintoma.B_Flujos, TipoSintoma.D_Dolor };
+        enfermedades[Enfermedades.SARPULLIDO] = new TipoSintoma[] { TipoSintoma.A_Calor, TipoSintoma.B_Flujos, TipoSintoma.C_Cuerpo };
+        enfermedades[Enfermedades.RAMAS_BRAZOS] = new TipoSintoma[] {
+    TipoSintoma.C_Cuerpo,
+    TipoSintoma.D_Dolor,
+    TipoSintoma.E_Conducta
+};
+
+        enfermedades[Enfermedades.OTITIS] = new TipoSintoma[] {
+    TipoSintoma.B_Flujos,
+    TipoSintoma.D_Dolor,
+    TipoSintoma.E_Conducta
+};
+
+        enfermedades[Enfermedades.OJOS_CARACOL] = new TipoSintoma[] {
+    TipoSintoma.A_Calor,
+    TipoSintoma.E_Conducta,
+    TipoSintoma.B_Flujos
+};
+
+        enfermedades[Enfermedades.LEPRA] = new TipoSintoma[] {
+    TipoSintoma.C_Cuerpo,
+    TipoSintoma.D_Dolor,
+    TipoSintoma.A_Calor
+};
+
+        enfermedades[Enfermedades.BICHOS_OJOS] = new TipoSintoma[] {
+    TipoSintoma.B_Flujos,
+    TipoSintoma.C_Cuerpo,
+    TipoSintoma.E_Conducta
+};
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public TipoSintoma[] getSintomas(Enfermedades enfermedad)
+    {
+        return enfermedades[enfermedad];
+    }
+}
